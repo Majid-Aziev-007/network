2547227317a4871ba8cb18b53161b771caeae5b1
Majid <108188311+Majid-Aziev-007@users.noreply.github.com>
2023-08-10 12:40:27 +0300

    Commit Models
    - Добавлена модель Meeting (встреча)
    - Добавлена модель Topic (Тематика)

--- network/meetings/models.py
+++ network/meetings/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Topic(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Meeting(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    address = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='meetings'
+    )
+    topic = models.ForeignKey(
+        Topic,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='topic'
+    )
