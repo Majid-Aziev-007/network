c866dbac15106b3cfac33cd230df974d101a18c0
Majid <108188311+Majid-Aziev-007@users.noreply.github.com>
2023-08-10 14:27:54 +0300

    Commit - Login Logout SignUp
    - Добавлены Login / Logout / SignUp

--- network/network/settings.py
+++ network/network/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.join(BASE_DIR, 'static'), 'static')
+
+LOGIN_URL = 'users:login'
+LOGIN_REDIRECT_URL = 'meetings:index'
