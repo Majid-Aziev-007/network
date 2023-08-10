8ca7f7d5cf62eff5f91620c6a6477192ad6c9f6b
Majid <108188311+Majid-Aziev-007@users.noreply.github.com>
2023-08-10 13:09:18 +0300

    Commit Important
    - Добавление в urls.py ссылку на "/"
    - Добавление в views.py index()
    - Добавление в settings.py настройки для templates
    
    - Добавление base.html
    - Добавление footer.html
    - Добавление header.html
    - Добавление index.html

--- network/meetings/views.py
+++ network/meetings/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Meeting, Topic
 
-# Create your views here.
+def index(request):
+    meeting_list = Meeting.objects.all().order_by('-pub_date')
+
+    context = {
+        'meeting_list': meeting_list
+    }
+
+    return render(request, 'meetings/index.html', context)
