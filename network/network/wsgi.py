08e294cd2156a2bf146d0ac774c3b24917d26f51
Majid <108188311+Majid-Aziev-007@users.noreply.github.com>
2023-08-10 19:44:05 +0300

    Commit Hosting
    - Вывод проекта на хостинг

--- network/network/wsgi.py
+++ network/network/wsgi.py
@@ -1,16 +1,15 @@
-"""
-WSGI config for network project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
-
+# -*- coding: utf-8 -*-
 import os
+import sys
+import platform
+#путь к проекту, там где manage.py
+sys.path.insert(0, '/home/c/cd80137/network/public_html/network')
+#путь к фреймворку, там где settings.py
+sys.path.insert(0, '/home/c/cd80137/network/public_html/network/network')
+#путь к виртуальному окружению myenv
+sys.path.insert(0, '/home/c/cd80137/network/venv/lib/python{0}/site-packages'.format(platform.python_version()[0:3]))
+#sys.path.insert(0, '/home/c/cd80137/network/venv/lib/python3.6/site-packages')
+os.environ["DJANGO_SETTINGS_MODULE"] = "network.settings"
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'network.settings')
-
 application = get_wsgi_application()
