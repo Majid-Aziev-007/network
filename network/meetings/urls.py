7c07327d50514bcb09e1c6cbd6f3d8d1cb887897
Majid <108188311+Majid-Aziev-007@users.noreply.github.com>
2023-08-11 21:22:45 +0300

    Commit Presence
    - Добавлена новая модель Presence, которая будет показывать заявки на нетворк

--- network/meetings/urls.py
+++ network/meetings/urls.py
@@ -8,4 +8,14 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('topic/<slug:slug>/', views.topic_meetings, name='topic_list'),
     path('meetings/<int:meeting_id>/', views.meeting_detail, name='meeting_detail'),
+    path(
+        'meetings/<int:meeting_id>/presence/',
+        views.meeting_presence,
+        name='meeting_presence'
+    ),
+    path(
+        'meetings/<int:meeting_id>/not_presence/',
+        views.meeting_not_presence,
+        name='meeting_not_presence'
+    ),
 ]
--- network/meetings/views.py
+++ network/meetings/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Meeting, Topic
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Meeting, Topic, Presence
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 def index(request):
     meeting_list = Meeting.objects.all().order_by('-pub_date')
@@ -24,8 +28,39 @@ def topic_meetings(request, slug):
 def meeting_detail(request, meeting_id):
 
     meeting = get_object_or_404(Meeting, pk=meeting_id)
+    presence_quantity = Presence.objects.filter(meeting=meeting).all().count()
+
+    if request.user.is_authenticated:
+        presence = Presence.objects.filter(
+            user=User.objects.get(username=request.user.username),
+            meeting=meeting
+        ).exists()
+    else:
+        presence = None
 
     context = {
         'meeting': meeting,
+        'presence': presence,
+        'presence_quantity': presence_quantity
     }
+
     return render(request, 'meetings/meeting_detail.html', context)
+
+@login_required
+def meeting_presence(request, meeting_id):
+
+    meeting = get_object_or_404(Meeting, pk=meeting_id)
+
+    Presence.objects.get_or_create(user=request.user, meeting=meeting)
+
+    return redirect('meetings:meeting_detail', meeting_id)
+
+@login_required
+def meeting_not_presence(request, meeting_id):
+
+    user = get_object_or_404(User, username=request.user.username)
+    meeting = get_object_or_404(Meeting, pk=meeting_id)
+
+    get_object_or_404(Presence, user=user, meeting=meeting).delete()
+
+    return redirect('meetings:meeting_detail', meeting_id)
