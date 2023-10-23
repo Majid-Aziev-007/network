8b5ad57dbbcd441523d617ea71f8d891ecfe3d47
Majid <108188311+Majid-Aziev-007@users.noreply.github.com>
2023-10-23 18:45:26 +0300

    Commit

--- network/panelcreator/models.py
+++ network/panelcreator/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Creator(models.Model):
+    """ Креатор """
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
